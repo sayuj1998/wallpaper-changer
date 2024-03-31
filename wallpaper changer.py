c62b10f0865fd68b3830bfbba1b0490ac773dfb9
sayuj1998 <sayuj21@gmail.com>
2024-03-31 17:17:35 -0600

    organizing

---
+++ wallpaper changer.py
@@ -0,0 +1,9 @@
+# find the wallpaper file
+# os.walk the files in that folder
+# randomize it using random module
+# make it an hour by default
+# have optional cmd line argument that will ask how often this script should run (minutes)
+# use subprocess to use schtasks which should run this script depending on user input on cmd line
+# file called README.md that explains what this script is and how to use it
+
+import os, sys, random, subprocess
